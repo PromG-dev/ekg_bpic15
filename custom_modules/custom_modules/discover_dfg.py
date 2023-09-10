67980db8c2a30f5c67867f88bb25aa422a7bf7bb
Ava-S <ava.swevels@gmail.com>
2023-09-10 11:02:44 +0200

    Update method to use schema (semantic header)

--- custom_modules/custom_modules/discover_dfg.py
+++ custom_modules/custom_modules/discover_dfg.py
@@ -1,4 +1,4 @@
-from promg import DatabaseConnection
+from promg import DatabaseConnection, SemanticHeader
 from promg import Performance
 from custom_modules.custom_queries.discovery_dfg import DiscoverDFGQueryLibrary as ql
 
@@ -8,21 +8,23 @@ class DiscoverDFG:
         self.connection = DatabaseConnection()
 
     @Performance.track()
-    def discover_dfg_for_entity(self, df_label:str,dfc_label:str,df_threshold:int=0,relative_df_threshold:float=0.0,corr_label:str='CORR'):
-        if relative_df_threshold==0.0:
+    def discover_dfg_for_entity(self, entity_str: str, df_threshold: int = 0, relative_df_threshold: float = 0.0):
+        entity = SemanticHeader().get_entity(entity_type=entity_str)
+
+        if relative_df_threshold == 0.0:
             self.connection.exec_query(ql.aggregate_df_relations,
-                                    **{
-                                            "corr_label": corr_label,
-                                            "df_label": df_label,
-                                            "dfc_label": dfc_label,
-                                            "df_threshold": df_threshold
-                                        })
+                                       **{
+                                           "corr_label": entity.get_corr_type_strings(),
+                                           "df_label": entity.get_df_label(),
+                                           "dfc_label": entity.get_df_a_label(),
+                                           "df_threshold": df_threshold
+                                       })
         else:
             self.connection.exec_query(ql.aggregate_df_relations_heuristic,
-                                    **{
-                                            "corr_label": corr_label,
-                                            "df_label": df_label,
-                                            "dfc_label": dfc_label,
-                                            "df_threshold": df_threshold,
-                                            "relative_df_threshold": relative_df_threshold
-                                        })
+                                       **{
+                                           "corr_label": entity.get_corr_type_strings(),
+                                           "df_label": entity.get_df_label(),
+                                           "dfc_label": entity.get_df_a_label(),
+                                           "df_threshold": df_threshold,
+                                           "relative_df_threshold": relative_df_threshold
+                                       })
