e2ced48602f792b9f2695fc1634cffacc48c84c4
Ava-S <ava.swevels@gmail.com>
2023-09-10 11:02:16 +0200

    Update method to use schema (semantic header) Delete create_df_edges_for_relations functionality (is supported in PromG)

--- custom_modules/custom_modules/df_interactions.py
+++ custom_modules/custom_modules/df_interactions.py
@@ -1,4 +1,4 @@
-from promg import DatabaseConnection
+from promg import DatabaseConnection, SemanticHeader
 from promg import Performance
 from custom_modules.custom_queries.df_interactions import InferDFInteractionsQueryLibrary as ql
 
@@ -6,21 +6,14 @@ from custom_modules.custom_queries.df_interactions import InferDFInteractionsQue
 class InferDFInteractions:
     def __init__(self):
         self.connection = DatabaseConnection()
-
-    @Performance.track()
-    def create_df_edges_for_relations(self, entity_labels:str,df_entity:str,entity_type:str,corr_type:str='CORR'):
-        self.connection.exec_query(ql.create_df_edges_for_relations,
-                                   **{
-                                       "entity_labels": entity_labels,
-                                       "corr_type": corr_type,
-                                       "df_entity": df_entity,
-                                       "entity_type": entity_type
-                                   })
         
     @Performance.track()
-    def delete_parallel_directly_follows_derived(self, df_entity:str,df_original_entity:str):
+    def delete_parallel_directly_follows_derived(self, entity_str:str,original_entity_str:str):
+        entity = SemanticHeader().get_entity(entity_type=entity_str)
+        original_entity = SemanticHeader().get_entity(entity_type=original_entity_str)
+
         self.connection.exec_query(ql.delete_parallel_directly_follows_derived,
                                    **{
-                                        "df_entity": df_entity,
-                                        "df_original_entity": df_original_entity
+                                        "entity": entity,
+                                        "original_entity": original_entity
                                    })
