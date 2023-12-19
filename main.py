a13506626d3c8069bb071e2dc5d209a0b2131e8f
Ava-S <ava.swevels@gmail.com>
2023-12-19 15:40:07 +0100

    Create config file and update main.py

--- main.py
+++ main.py
@@ -1,46 +1,32 @@
-import os
 from datetime import datetime
-from pathlib import Path
 
 from promg import SemanticHeader, OcedPg
 from promg import DatabaseConnection
-from promg import authentication
 from promg import DatasetDescriptions
+from promg import Configuration
 
 from promg import Performance
 from promg.modules.db_management import DBManagement
 from promg.modules.task_identification import TaskIdentification
-from promg.modules.process_discovery import ProcessDiscovery
 
 from custom_modules.custom_modules.df_interactions import InferDFInteractions
 from custom_modules.custom_modules.delay_analysis import PerformanceAnalyzeDelays
 from custom_modules.custom_modules.discover_dfg import DiscoverDFG
 
-# several steps of import, each can be switch on/off
+
 from colorama import Fore
 
-dataset_name = 'BPIC16'
-use_sample = False
-batch_size = 10000
-use_preprocessed_files = False
+config = Configuration()
+semantic_header = SemanticHeader.create_semantic_header(config.semantic_header_path)
+dataset_descriptions = DatasetDescriptions(config.dataset_description_path)
 
-semantic_header_path = Path(f'json_files/{dataset_name}.json')
-
-semantic_header = SemanticHeader.create_semantic_header(semantic_header_path)
-
-ds_path = Path(f'json_files/{dataset_name}_DS.json')
-dataset_descriptions = DatasetDescriptions(ds_path)
-
+# several steps of import, each can be switch on/off
 step_clear_db = True
 step_populate_graph = True
 step_delete_parallel_df = True
 step_discover_model = True
 step_build_tasks = True
 step_infer_delays = True
-
-verbose = False
-credentials_key = authentication.Connections.LOCAL
-import_directory = "TODO"
 
 
 def main() -> None:
@@ -50,10 +36,8 @@ def main() -> None:
     """
     print("Started at =", datetime.now().strftime("%H:%M:%S"))
 
-    db_connection = DatabaseConnection.set_up_connection_using_key(key=credentials_key,
-                                                                   verbose=verbose)
-    performance = Performance.set_up_performance(dataset_name=dataset_name,
-                                                 use_sample=use_sample)
+    db_connection = DatabaseConnection.set_up_connection_using_config(config=config)
+    performance = Performance.set_up_performance(config=config)
     db_manager = DBManagement()
 
     if step_clear_db:
@@ -62,19 +46,19 @@ def main() -> None:
         db_manager.set_constraints()
 
     if step_populate_graph:
-        if use_preprocessed_files:
+        if config.use_preprocessed_files:
             print(Fore.RED + '💾 Preloaded files are used!' + Fore.RESET)
         else:
             print(Fore.RED + '📝 Importing and creating files' + Fore.RESET)
 
         oced_pg = OcedPg(dataset_descriptions=dataset_descriptions,
-                         use_sample=use_sample,
-                         use_preprocessed_files=use_preprocessed_files,
-                         import_directory=import_directory)
+                         use_sample=config.use_sample,
+                         use_preprocessed_files=config.use_preprocessed_files)
+        # import_directory=config.import_directory)
         oced_pg.load_and_transform()
         oced_pg.create_df_edges()
 
-    if dataset_name == 'BPIC17':
+    if config.db_name == 'BPIC17':
         if step_delete_parallel_df:
             print(Fore.RED + 'Inferring DF over relations between objects.' + Fore.RESET)
             infer_df_interactions = InferDFInteractions()
